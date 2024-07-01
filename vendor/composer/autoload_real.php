b8a739cf555078b71721febc97e1260563f5f018
Luckas Simplicio <luckas.ribeiro04@gmail.com>
2024-07-01 16:41:34 -0300

    Adicionando dependencias do slim/slim e da PRS-7 ao projeto

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitf776ddba14a99ccd886d73069191d213
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitf776ddba14a99ccd886d73069191d213', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitf776ddba14a99ccd886d73069191d213', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInitf776ddba14a99ccd886d73069191d213
         call_user_func(\Composer\Autoload\ComposerStaticInitf776ddba14a99ccd886d73069191d213::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitf776ddba14a99ccd886d73069191d213::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
